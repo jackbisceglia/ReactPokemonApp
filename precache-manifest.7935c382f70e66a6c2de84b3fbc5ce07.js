f92048b842ad129c74c555ca99149ac8e5f7c60b
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-05-01 23:26:54 -0400

    Updates

---
+++ precache-manifest.7935c382f70e66a6c2de84b3fbc5ce07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861974ce0d17e18607dc3bb413af1d04",
+    "url": "/ReactPokemonApp/index.html"
+  },
+  {
+    "revision": "fba3749c7483bd07ee89",
+    "url": "/ReactPokemonApp/static/js/2.652a47fd.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/ReactPokemonApp/static/js/2.652a47fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd518499bf6f2d82dd5",
+    "url": "/ReactPokemonApp/static/js/main.d604758d.chunk.js"
+  },
+  {
+    "revision": "b816ae7255e23b305acf",
+    "url": "/ReactPokemonApp/static/js/runtime-main.59b41f95.js"
+  }
+]);
