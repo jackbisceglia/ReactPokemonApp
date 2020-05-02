5389b9de08f444e72ca409edbed75d1e9d47faae
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-05-01 21:47:56 -0400

    Pagination styling updated with Semantic UI

--- src/pagination.js
+++ src/pagination.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 
-export default function Pagination( {goToNextPage, goToPrevPage} ) {
+
+export default function Pagination({ goToNextPage, goToPrevPage }) {
     return (
-        <div>
-            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
-            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+        <div style={{width: '15vw', display: 'flex', justifyContent: 'center', margin : '25px'}}>
+            {goToPrevPage &&
+                <Button animated onClick={goToPrevPage}>
+                    <Button.Content visible>Prev</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name='arrow left' />
+                    </Button.Content>
+                </Button>
+            }
+
+            {goToNextPage &&
+                <Button animated onClick={goToNextPage}>
+                    <Button.Content visible>Next</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name='arrow right' />
+                    </Button.Content>
+                </Button>
+            }
+
         </div>
     )
 }
